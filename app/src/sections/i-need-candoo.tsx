071dc864730c3788accf9a2e4345abbc1643e963
David Thorn <david.thorn221278@googlemail.com>
2023-11-24 15:48:25 +0100

    Added services to i need candoo section

--- app/src/sections/i-need-candoo.tsx
+++ app/src/sections/i-need-candoo.tsx
@@ -1,3 +1,29 @@
+interface ServiceInterface {
+    caption: string
+    title: string
+    buttonText: string
+    imageSource: string
+}
+
+const services: ServiceInterface[] = [
+    {
+        caption: "Myself",
+        title: "Get the help you need right now.",
+        buttonText: "Get started",
+        imageSource: "https://placehold.co/700x500"
+    },
+    {
+        caption: "My loved ones",
+        title: "Give the help your loved ones need.",
+        buttonText: "Get started",
+        imageSource: "https://placehold.co/700x500"
+    }, {
+        caption: "My residents, clients and members",
+        title: "Learn how Candoo partners with senior living communities, health plans, healthcare & social service & government agencies.",
+        buttonText: "Get started",
+        imageSource: "https://placehold.co/700x500"
+    }
+]
 
 function INeedCandooSection() {
 
@@ -8,43 +34,20 @@ function INeedCandooSection() {
                     <h1 className="display-4 text-primary">I need Candoo Tech for…</h1>
                 </div>
                 <div className="row">
-                    <div className="col">
-                        <div className="text-center">
-                            <img src="/images/card-image1.jpg" className="img-fluid" />
-                            <h3>Myself</h3>
-                        </div>
-                        <div className="mt-2 p-2">
-                            <div className="d-flex flex-row justify-content-start align-items-center">
-                                <span className="flex-grow-1 text-align-left pe-2">Get the help you need right now.</span>
-                                <div className="btn btn-primary text-light text-center">Get Started</div>
+                    {services.map(service => (
+                        <div className="col">
+                            <div className="text-center">
+                                <img src={service.imageSource} className="img-fluid" />
+                                <h3>{service.caption}</h3>
+                            </div>
+                            <div className="mt-2 p-2">
+                                <div className="d-flex flex-row justify-content-start align-items-center">
+                                    <span className="flex-grow-1 text-align-left pe-2">{service.title}</span>
+                                    <div className="btn btn-primary text-light text-center">{service.buttonText}</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="col">
-                        <div className="text-center">
-                            <img src="/images/card-image2.jpg" className="img-fluid" />
-                            <h3>My loved ones</h3>
-                        </div>
-                        <div className="">
-                            <div className="d-flex flex-row justify-content-center align-items-start">
-                                <span className="flex-grow-1 text-align-left">Give the help your loved ones need.</span>
-                                <div className="btn btn-primary text-light text-center">Get Started</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="">
-                            <img src="/images/card-image3.jpg" className="img-fluid" />
-                            <h3>My residents, clients & members</h3>
-                        </div>
-                        <div className="">
-                            <div className="d-flex flex-row justify-content-center align-items-start">
-                                <span className="flex-grow-1 text-align-left pe-2">Learn how Candoo partners with senior living
-                                    communities, health plans, healthcare & social service & government agencies.</span>
-                                <div className="btn btn-primary text-light">Get Started</div>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
