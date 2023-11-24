3b756f9687b0bb265d7274ae57ac56987aadc8b2
David Thorn <david.thorn221278@googlemail.com>
2023-11-24 14:17:41 +0100

    Removed old function from file

--- app/src/components/index.tsx
+++ app/src/components/index.tsx
@@ -2,15 +2,6 @@ import { CardImageLeft } from "./card-image-left"
 import { CardImageRight } from "./card-image-right"
 import { Section } from "./section"
 
-function Tset() {
-    return (
-        <>
-
-        </>
-    )
-}
-
-
 export {
     CardImageLeft,
     CardImageRight,
